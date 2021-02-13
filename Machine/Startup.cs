1b1bb9814d5f7bb515acde610dd44cc3315a3649
MWalter777 <waltermarrom777@gmail.com>
2021-02-13 09:49:47 -0600

    First commit

---
+++ Machine/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Machine.Startup))]
+namespace Machine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
